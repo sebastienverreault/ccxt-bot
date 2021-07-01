ed51d162809b496a19195bd7d2ff52c327e8e796
Sebastien Verreault <sebver@pm.me>
2021-07-01 23:05:36 +0900

    Refactor, debug, lint and upgrade

--- src/GenericExchange.ts
+++ src/GenericExchange.ts
@@ -1,228 +1,218 @@
-import ccxt, { ExchangeId, Order } from "ccxt";
-import fs from "fs";
-import dateFormat from "dateformat";
+import ccxt, { ExchangeId, Order } from "ccxt"
+import { sleep } from "./utils"
+import fs from "fs"
+import dateFormat from "dateformat"
 import path from "path"
 
-const DATE_FORMAT_STRING = "yyyymmdd.HHMMss";
-const TEMP_DIR = 'temp-data';
+const DATE_FORMAT_STRING = "yyyymmdd.HHMMss"
+const TEMP_DIR = "temp-data"
 
 export class ApiConfig {
   constructor(
     public apiKey: string | undefined,
     public secret: string | undefined,
-    public password: string | undefined
-  ) {
-    ;
-  };
+    public password: string | undefined,
+  ) {}
+}
+
+export enum SupportedExchanges {
+  FTX = "ftx",
+  OKEX = "okex5",
 }
 
 export class GenericExchange {
   dateFormatString
-  tempDir;
-  exchangeName;
-  exchange;
-  symbol;
+  tempDir
+  exchangeName
+  exchange
+  symbol
 
   // public ccxt;
 
-  constructor(
-    exchangeName: string,
-    apiConfig: ApiConfig,
-    symbol: string
-  ) {
-    this.dateFormatString = DATE_FORMAT_STRING;
+  constructor(exchangeName: string, apiConfig: ApiConfig, symbol: string) {
+    this.dateFormatString = DATE_FORMAT_STRING
     this.tempDir = TEMP_DIR
-    this.exchangeName = exchangeName;
-    this.symbol = symbol;
-    const exchangeId = this.exchangeName as ExchangeId;
-    const exchangeClass = ccxt[exchangeId];
+    this.exchangeName = exchangeName
+    this.symbol = symbol
+    const exchangeId = this.exchangeName as ExchangeId
+    const exchangeClass = ccxt[exchangeId]
     this.exchange = new exchangeClass({
       apiKey: apiConfig.apiKey,
       secret: apiConfig.secret,
       password: apiConfig.password,
-    });
+    })
     // this.ccxt = this.exchange;
-    console.log(this.exchange.requiredCredentials);
-    console.log(this.exchange.checkRequiredCredentials());
+    console.log(this.exchange.requiredCredentials)
+    this.exchange.checkRequiredCredentials()
   }
 
   public GetSymbol() {
     // Should resolve best instrument to used given the exchange and the market conditions.
-    const optimalSymbol = this.symbol;
-    this.symbol = optimalSymbol;
-    return this.symbol;
+    const optimalSymbol = this.symbol
+    this.symbol = optimalSymbol
+    return this.symbol
   }
 
   public has() {
-    const has = this.exchange.has;
-    const data = JSON.stringify(has);
-
-    const datetime = dateFormat(new Date(), this.dateFormatString);
-    const filename = path.join(this.tempDir, `${this.exchangeName}.has.${datetime}.json`)
-    fs.writeFile(filename, data, function (err) {
-      if (err) return console.log(err);
-    });
+    const has = this.exchange.has
+    this.writeFile(has, "has")
+    return has
   }
 
   public has2() {
-    const has = this.exchange.has;
-    return has;
+    const has = this.exchange.has
+    return has
   }
 
   public name() {
-    const name = this.exchange.name;
-    return name;
+    const name = this.exchange.name
+    return name
   }
 
   async fetchDepositAddress(currency: string) {
     const address = await this.exchange.fetchDepositAddress(currency)
-    const data = JSON.stringify(address);
-    // console.log(data);
-
-    const datetime = dateFormat(new Date(), this.dateFormatString);
-    const filename = path.join(this.tempDir, `${this.exchangeName}.deposit.address.${datetime}.json`)
-    fs.writeFile(filename, data, function (err) {
-      if (err) return console.log(err);
-    });
+    this.writeFile(address, "deposit.address")
+    return address
   }
 
   public async withdraw(currency: string, amount: number, address: string) {
     const withdrawalResult = await this.exchange.withdraw(currency, amount, address)
-    return withdrawalResult;
+    return withdrawalResult
   }
 
-  public async createOrder(symbol: string, type: Order['type'], side: Order['side'], amount: number) {
+  public async createOrder(
+    symbol: string,
+    type: Order["type"],
+    side: Order["side"],
+    amount: number,
+  ) {
     const order = await this.exchange.createOrder(symbol, type, side, amount)
-    return order;
+    return order
   }
 
   public async fetchOrder(id: string) {
     const order = await this.exchange.fetchOrder(id, this.symbol)
-    return order;
+    return order
   }
 
   public async getPositions() {
-    const positions = await this.exchange.fetchPositions();
-    const data = JSON.stringify(positions);
-    // console.log(`positions object: ${data}`);
-
-    const datetime = dateFormat(new Date(), this.dateFormatString);
-    const filename = path.join(this.tempDir, `${this.exchangeName}.positions.${datetime}.json`)
-    fs.writeFile(filename, data, function (err) {
-      if (err) return console.log(err);
-    });
+    const positions = await this.exchange.fetchPositions()
+    this.writeFile(positions, "positions")
+    return positions
   }
 
   public async fetchBalance() {
-    const balances = await this.exchange.fetchBalance();
-    const data = JSON.stringify(balances.info);
-    // console.log(`balances: ${data}`);
-
-    const datetime = dateFormat(new Date(), this.dateFormatString);
-    const filename = path.join(this.tempDir, `${this.exchangeName}.balances.${datetime}.json`)
-    fs.writeFile(filename, data, function (err) {
-      if (err) return console.log(err);
-    });
+    const balances = await this.exchange.fetchBalance()
+    this.writeFile(balances, "balances")
+    return balances
   }
 
   public async getBtcSpot() {
-    this.getInstrument("BTC", "spot");
+    const instrument = await this.getInstrument("BTC", "spot")
+    return instrument
   }
 
   public async getBtcFutures() {
-    this.getInstrument("BTC", "future");
+    const instrument = await this.getInstrument("BTC", "future")
+    return instrument
   }
 
   public async getInstrument(base: string, type: string) {
-    const markets = await this.exchange.loadMarkets(true);
-    let data = "";
+    const markets = await this.exchange.loadMarkets(true)
+    let data = ""
 
-    for (let symbol in markets) {
+    for (const symbol in markets) {
       try {
         if (!symbol.includes("MOVE")) {
-          const market = markets[symbol];
+          const market = markets[symbol]
           if (market["base"] === base || market["quote"] === base)
             if (market["type"] === type || market["type"] === type + "s") {
-              const ticker = await this.exchange.fetchTicker(symbol);
-              data += `symbol = ${symbol}\n`;
-              data += `Market = ${JSON.stringify(market)}\n`;
-              data += `Ticker = ${JSON.stringify(ticker)}\n`;
-              await (ccxt as any).sleep(this.exchange.rateLimit);
+              const ticker = await this.exchange.fetchTicker(symbol)
+              data += `symbol = ${symbol}\n`
+              data += `Market = ${JSON.stringify(market)}\n`
+              data += `Ticker = ${JSON.stringify(ticker)}\n`
+              await sleep(this.exchange.rateLimit)
             }
         }
       } catch (error) {
-        data += `error = ${error}\n`;
+        data += `error = ${error}\n`
       }
     }
 
-    const datetime = dateFormat(new Date(), this.dateFormatString);
-    const filename = path.join(this.tempDir, `${this.exchangeName}.${type}.${datetime}.json`)
-    fs.writeFile(filename, data, function (err) {
-      if (err) return console.log(err);
-    });
+    this.writeFileRaw(data, type)
+
+    return markets
   }
 
   public async getNextFundingRate(symbol: string) {
     if (this.exchangeName === "ftx") {
-      const result = await this.exchange.publicGetFuturesFutureNameStats({ future_name: symbol });
-      return result;
+      const arg = { future_name: symbol }
+      const result = await this.exchange.publicGetFuturesFutureNameStats(arg)
+      this.writeFile(result, "funding.rate")
+      return result
     } else if (this.exchangeName === "okex" || this.exchangeName === "okex5") {
-      // following method does not exist...
-      const result = await this.exchange.publicGetFuturesFutureNameStats({ future_name: symbol });
-      return result;
+      const arg = { instId: symbol }
+      const result = await this.exchange.publicGetPublicFundingRate(arg)
+      this.writeFile(result, "funding.rate")
+      return result
     }
   }
 
   public async privateGetAccount() {
     if (this.exchangeName === "ftx") {
-      const result = await this.exchange.privateGetAccount();
-      return result;
+      const result = await this.exchange.privateGetAccount()
+      return result
     } else if (this.exchangeName === "okex" || this.exchangeName === "okex5") {
       // following method does not exist...
-      const result = await this.exchange.privateGetAccount();
-      return result;
+      const result = await this.exchange.privateGetAccount()
+      return result
     }
   }
 
-  // Sub-class this to implement exchange specific method
-  public async getStats() {
-    const datetime = dateFormat(new Date(), this.dateFormatString);
-    const filename = path.join(this.tempDir, `${this.exchangeName}.stats.${datetime}.json`);
-    let data = "";
-    if (this.exchangeName === "ftx") {
-      data = JSON.stringify(
-        // this.exchange.fetchFundingFees({
-        this.exchange.publicGetFuturesFutureNameStats({
-          future_name: this.symbol,
-        })
-      );
-    } else if (this.exchangeName === "okex" || this.exchangeName === "okex5") {
-      data = JSON.stringify(
-        this.exchange.fetchFundingFees({
-          future_name: this.symbol,
-        })
-      );
-    }
-    fs.writeFile(filename, data, function (err) {
-      if (err) return console.log(err);
-    });
-  }
+  // // Sub-class this to implement exchange specific method
+  // public async getStats() {
+  //   let data = ""
+  //   if (this.exchangeName === "ftx") {
+  //     data = JSON.stringify(
+  //       this.exchange.publicGetFuturesFutureNameStats({
+  //         future_name: this.symbol,
+  //       }),
+  //     )
+  //   } else if (this.exchangeName === "okex" || this.exchangeName === "okex5") {
+  //     data = JSON.stringify(
+  //       this.exchange.publicGetPublicFundingRate({
+  //         future_name: this.symbol,
+  //       }),
+  //     )
+  //   }
+  //   this.writeFileRaw(data, "stats")
+  // }
 
   public async getMethods() {
-    const datetime = dateFormat(new Date(), this.dateFormatString);
-    const filename = path.join(this.tempDir, `${this.exchangeName}.methods.${datetime}.json`);
-    const data = Object.keys(this.exchange).join("\n").toString();
-    fs.writeFile(filename, data, function (err) {
-      if (err) return console.log(err);
-    });
+    const methods = Object.keys(this.exchange)
+    const data = methods.join("\n").toString()
+    this.writeFileRaw(data, "methods")
+    return methods
   }
 
   public async getMarkets() {
-    const markets = await this.exchange.loadMarkets();
-    const datetime = dateFormat(new Date(), this.dateFormatString);
-    const filename = path.join(this.tempDir, `${this.exchangeName}.market.${datetime}.json`);
-    fs.writeFile(filename, JSON.stringify(markets), function (err) {
-      if (err) return console.log(err);
-    });
+    const markets = await this.exchange.loadMarkets()
+    this.writeFile(markets, "market")
+    return markets
+  }
+
+  private writeFileRaw(rawData, type) {
+    const datetime = dateFormat(new Date(), this.dateFormatString)
+    const filename = path.join(
+      this.tempDir,
+      `${this.exchangeName}.${type}.${datetime}.json`,
+    )
+    fs.writeFile(filename, rawData, function (err) {
+      if (err) return console.log(err)
+    })
+  }
+
+  private writeFile(data, type) {
+    this.writeFileRaw(JSON.stringify(data), type)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,63 @@
-import { GenericExchange, ApiConfig } from "./GenericExchange";
+import { GenericExchange, SupportedExchanges, ApiConfig } from "./GenericExchange"
 
-enum Exchanges {
-  FTX = "ftx",
-  OKEX = "okex5",
-}
+// enum Exchanges {
+//   FTX = "ftx",
+//   OKEX = "okex5",
+// }
 
-const activeExchangeId = Exchanges.OKEX;
+const activeExchangeId = SupportedExchanges.OKEX
 
-const activeApiConfig = new ApiConfig(undefined, undefined, undefined);
+const activeApiConfig = new ApiConfig(undefined, undefined, undefined)
 
-switch (activeExchangeId as Exchanges) {
-  case Exchanges.FTX:
+switch (activeExchangeId as SupportedExchanges) {
+  case SupportedExchanges.FTX:
     activeApiConfig.apiKey = process.env.FTX_KEY
     activeApiConfig.secret = process.env.FTX_SECRET
     activeApiConfig.password = process.env.FTX_PASSWORD
-    break;
+    break
 
-  case Exchanges.OKEX:
+  case SupportedExchanges.OKEX:
     activeApiConfig.apiKey = process.env.OKEX_KEY
     activeApiConfig.secret = process.env.OKEX_SECRET
     activeApiConfig.password = process.env.OKEX_PASSWORD
-    break;
+    break
 
   default:
-    break;
+    break
 }
 
-const ftxExchangeId = "ftx";
+const ftxExchangeId = "ftx"
 // const ftxApiKey = process.env.SEB_FTX_KEY;
 // const ftxApiSecret = process.env.SEB_FTX_SECRET;
 // const ftxPassword = process.env.SEB_FTX_PASSWORD;
-const ftxApiKey = process.env.NIC_FTX_KEY;
-const ftxApiSecret = process.env.NIC_FTX_SECRET;
-const ftxPassword = process.env.NIC_FTX_PASSWORD;
-const ftxApiConfig = new ApiConfig(ftxApiKey, ftxApiSecret, ftxPassword);
+const ftxApiKey = process.env.NIC_FTX_KEY
+const ftxApiSecret = process.env.NIC_FTX_SECRET
+const ftxPassword = process.env.NIC_FTX_PASSWORD
+const ftxApiConfig = new ApiConfig(ftxApiKey, ftxApiSecret, ftxPassword)
 
-const okexExchangeId = "okex5";
-const okexApiKey = process.env.OKEX_KEY;
-const okexApiSecret = process.env.OKEX_SECRET;
-const okexPassword = process.env.OKEX_PASSWORD;
-const okexApiConfig = new ApiConfig(okexApiKey, okexApiSecret, okexPassword);
+const okexExchangeId = "okex5"
+const okexApiKey = process.env.OKEX_KEY
+const okexApiSecret = process.env.OKEX_SECRET
+const okexPassword = process.env.OKEX_PASSWORD
+const okexApiConfig = new ApiConfig(okexApiKey, okexApiSecret, okexPassword)
 // const okexApiKey = process.env.TRADE_OKEX_KEY;
 // const okexApiSecret = process.env.TRADE_OKEX_SECRET;
 // const okexPassword = process.env.TRADE_OKEX_PASSWORD;
 // const okexApiConfig = new ApiConfig(okexApiKey, okexApiSecret, okexPassword);
 
-const ftxSymbol = "BTC-PERP";
-const okexSymbol = "BTC/USDT";
+const ftxSymbol = "BTC-PERP"
+const okexSymbol = "BTC-USD-211231"
+// const okexSymbol = "BTC-USD-210709"
+// const okexSymbol = "BTC/USDT"
+const okexSwapSymbol = "BTC-USD-SWAP"
 
-function main() {
-  const ftx = new GenericExchange(ftxExchangeId, ftxApiConfig, ftxSymbol);
-  const okex = new GenericExchange(okexExchangeId, okexApiConfig, okexSymbol);
+async function main() {
+  const ftx = new GenericExchange(ftxExchangeId, ftxApiConfig, ftxSymbol)
+  const okex = new GenericExchange(okexExchangeId, okexApiConfig, okexSymbol)
   // const activeExchange = new GenericExchange(activeExchangeId, activeApiConfig, okexSymbol);
 
-  //   ftx.getBtcSpot();
-  //   okex.getBtcSpot();
+  // ftx.getBtcSpot();
+  // okex.getBtcSpot();
 
   // ftx.getBtcFutures();
   // okex.getBtcFutures();
@@ -62,25 +65,45 @@ function main() {
   // ftx.getMethods();
   // okex.getMethods();
 
-  // ftx.getStats();
-  // okex.getStats();
+  // const ftxStats = await ftx.getStats()
+  // const okexStats = await okex.getStats()
   // okex3.getStats();
 
   //   ftx.getMarkets();
   //   okex.getMarkets();
 
-  ftx.fetchBalance();
-  okex.fetchBalance();
+  // const ftxFundingRate = await ftx.getNextFundingRate(ftxSymbol)
+  // console.log(`NextFundingrate = ${JSON.stringify(ftxFundingRate)}`)
+  // console.log(
+  //   `NextFundingrate.nextFundingRate = ${ftxFundingRate.result.nextFundingRate}`,
+  // )
 
-  ftx.getPositions();
-  okex.getPositions();
+  try {
+    const okexFundingRate = await okex.getNextFundingRate(okexSwapSymbol)
+    console.log(`NextFundingrate = ${JSON.stringify(okexFundingRate)}`)
+    console.log(
+      `NextFundingrate.nextFundingRate = ${okexFundingRate.data[0].nextFundingRate}`,
+    )
+  } catch (error) {
+    console.log(error)
+  }
+
+  // const ftxBal = await ftx.fetchBalance();
+  // console.log(`balance.total.BTC = ${ftxBal.total.BTC}`)
+  // console.log(`balance.total.USD = ${ftxBal.total.USD}`)
+  // const okexBal = await okex.fetchBalance();
+  // console.log(`balance.total.BTC = ${okexBal.total.BTC}`)
+  // console.log(`balance.total.USDT = ${okexBal.total.USDT}`)
+
+  // ftx.getPositions();
+  // okex.getPositions();
 
   // ftx.has();
   // okex.has();
   // okex3.has();
 
-  ftx.fetchDepositAddress("BTC");
-  okex.fetchDepositAddress("BTC");
+  // ftx.fetchDepositAddress("BTC");
+  // okex.fetchDepositAddress("BTC");
 
   // console.log(activeExchange.name());
   // console.log(ftx.name());
@@ -90,4 +113,4 @@ function main() {
   // console.log(okex.has2());
 }
 
-main();
+main()
